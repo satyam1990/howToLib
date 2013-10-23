b2d94b46061d41dd4ee84c3cd5870b388c781583
mSatyam <motogeeeksatyam@gmail.com>
2013-10-23 17:36:20 +0530

    added headers for .c files

---
+++ david.h
@@ -0,0 +1,15 @@
+/***************************************************************************
+ *
+ * david.h
+ *
+ * Author: mSatyam
+ *
+ * October, 2013
+ *
+ * Contain prototype for functions defined in david.c
+ ***************************************************************************/
+
+/*
+ * sample display function for david
+ */
+void displayDavid();
---
+++ rob.h
@@ -0,0 +1,15 @@
+/***************************************************************************
+ *
+ * rob.h
+ *
+ * Author: mSatyam
+ *
+ * October, 2013
+ *
+ * Contain prototype for functions defined in rob.c
+ ***************************************************************************/
+
+/*
+ * sample display function for rob
+ */
+void displayRob();
